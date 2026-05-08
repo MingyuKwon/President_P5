49f6fde8f830506038049fef9de3a4955abfe343
Kwon Mingyu <kmgmg230601@gmail.com>
2026-05-09 00:19:00 +0900

    style: 내 플레이어 패널 위치 중앙 기준 -370px

--- client/js/game.js
+++ client/js/game.js
@@ -246,7 +246,7 @@ function renderSeats() {
     if (isMe) {
       // #my-area 자식으로 붙여야 브라우저 크기에 반응
       // 50% = #my-area 폭(= 뷰포트 폭) 기준
-      div.style.cssText = 'position:absolute; left:calc(50% - 400px); bottom:calc(100% + 100px); top:auto; transform:translateX(-50%);';
+      div.style.cssText = 'position:absolute; left:calc(50% - 370px); bottom:calc(100% + 100px); top:auto; transform:translateX(-50%);';
       myAreaEl.appendChild(div);
       return;
     }
