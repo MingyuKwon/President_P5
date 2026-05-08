208054883726094773b1ce4930337cd31feadd6e
권민규 [kmgmg2391601] <kmgmg2391601@nexon.co.kr>
2026-05-08 18:46:59 +0900

    game: 내 시트 좌측 위치에 max() 가드 추가로 화면 잘림 방지

--- client/js/game.js
+++ client/js/game.js
@@ -166,7 +166,7 @@ function renderSeats() {
     if (isMe) {
       // #my-area 자식으로 붙여야 브라우저 크기에 반응
       // 50% = #my-area 폭(= 뷰포트 폭) 기준
-      div.style.cssText = 'position:absolute; left:calc(50% - 600px); bottom:calc(100% + 50px); top:auto; transform:translate(-50%,0);';
+      div.style.cssText = 'position:absolute; left:max(8%, calc(50% - 600px)); bottom:calc(100% + 50px); top:auto; transform:translate(-50%,0);';
       myAreaEl.appendChild(div);
       return;
     }
