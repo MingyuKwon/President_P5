bd8885e2a8e172ea6b4919547fd11208b901d1f3
Kwon Mingyu <kmgmg230601@gmail.com>
2026-05-09 00:47:16 +0900

    style: 손패 끝 카드 드롭 8px → 20px

--- client/js/game.js
+++ client/js/game.js
@@ -283,7 +283,7 @@ function renderHand() {
 
     const angleDeg = n <= 1 ? 0 : (i - (n - 1) / 2) / (n - 1) * totalAngle;
     const maxHalf = totalAngle / 2;
-    const drop = maxHalf > 0 ? (angleDeg / maxHalf) ** 2 * 8 : 0;
+    const drop = maxHalf > 0 ? (angleDeg / maxHalf) ** 2 * 20 : 0;
     div.style.setProperty('--rot', `${angleDeg}deg`);
     div.style.setProperty('--drop', `${drop}px`);
 
