0694b70dfaa9f0154dcbba69fdf4ccc767b85fb0
Kwon Mingyu <kmgmg230601@gmail.com>
2026-05-10 02:01:27 +0900

    fix: 카드 교환 이미지 크기 30% 축소

--- client/js/game.js
+++ client/js/game.js
@@ -512,7 +512,7 @@ function showTaxExchangeImage() {
   });
   const img = document.createElement('img');
   img.src = '/Resource/UI/game-state/ExchangeCard.png';
-  Object.assign(img.style, { width: '38vw', height: '38vh', objectFit: 'contain' });
+  Object.assign(img.style, { width: '26.6vw', height: '26.6vh', objectFit: 'contain' });
   taxExchangeEl.appendChild(img);
   document.body.appendChild(taxExchangeEl);
 
