be5de36e5a6e237a370575617446444860e117fe
권민규 [kmgmg2391601] <kmgmg2391601@nexon.co.kr>
2026-05-20 17:21:20 +0900

    feat: HOST 환경변수로 바인딩 주소 설정 가능, 기본값 0.0.0.0 (외부 접속 허용)

--- server/server.js
+++ server/server.js
@@ -724,4 +724,5 @@ function publicPlayers(players) {
 }
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const HOST = process.env.HOST || '0.0.0.0';
+server.listen(PORT, HOST, () => console.log(`Server running on http://${HOST}:${PORT}`));
